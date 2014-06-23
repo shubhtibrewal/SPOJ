fdd09eb080e72b467622149d17cd598e1105cfc5
harsh tibrewal <harshtibrewal.nitdgp@gmail.com>
2014-06-24 02:07:49 +0530

    Modified solution

--- M_SEQ.cpp
+++ M_SEQ.cpp
@@ -11,9 +11,7 @@ int main()
     {
         long n;
         scanf("%ld", &n);
-        //printf("F'(%d)=%d\n", n, n*(n+1)*(n+2)/6);
-        //printf("G(%d)=%.8f\n", n, sqrt(float(4*n*(n+1)*(n+2)/3-4*(n-1)*n*(n+1)/3+1))/n);
-        printf("%.8f\n", sqrt(4.0*double(n+1)*double(n+2)/3/n-4.0*double(n-1)*double(n+1)/3/n+1.0/n/n));
+        printf("%.8f\n", sqrt(4.0*double(n+1)/n+1.0/n/n));
     }
     return 0;
 }
